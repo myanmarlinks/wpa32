55426726ae98b3859c5f723224fdcdb491ae2c7e
phpcrazy <phpcrazy@gmail.com>
2019-09-07 18:19:12 +0630

    Controller

---
+++ phpbasic/app/controller/controllers.php
@@ -0,0 +1,11 @@
+<?php
+
+function _HomeController() {
+    _get_view("home");
+}
+
+function _BlogController() {
+    _get_view("blog");
+}
+
+?>
--- phpbasic/public/index.php
+++ phpbasic/public/index.php
@@ -1,11 +1,18 @@
 <?php
 define("DD", realpath("../"));
 require DD . "/wpa32/functions.php";
+require DD . "/app/controller/controllers.php";
 if(!isset($_GET['page'])) {
     $page = "home";
 } else {
     $page = $_GET['page'];
 }
-_get_view($page);
+$controller = "_" . ucfirst($page) . "Controller";
+if(function_exists($controller)) {
+    call_user_func($controller);
+} else {
+    echo "404 Idiot!";
+}
+
 
 ?>
