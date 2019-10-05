f3c1939285b1b49c834b36e52ebfd9180c460cc6
phpcrazy <phpcrazy@gmail.com>
2019-10-05 17:26:07 +0630

    VueJS

--- phpbasic/app/controller/controllers.php
+++ phpbasic/app/controller/controllers.php
@@ -120,25 +120,17 @@ function _HomeController() {
                 $sql = "SELECT id, username, password FROM users WHERE username = ?";
                 
                 if($stmt = mysqli_prepare($link, $sql)){
-                    
                     mysqli_stmt_bind_param($stmt, "s", $param_username);
-                    
                     $param_username = $username;
                     
-                    
-                    if(mysqli_stmt_execute($stmt)){
-                        
+                    if(mysqli_stmt_execute($stmt)){    
                         mysqli_stmt_store_result($stmt);
                         if(mysqli_stmt_num_rows($stmt) == 1){  
-                            
                             mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                             if(mysqli_stmt_fetch($stmt)){
                                 
                                 if(password_verify($user_password, $hashed_password)){
-                                    
-                                    
                                     session_start();
-                                    
                                     $_SESSION["loggedin"] = true;
                                     $_SESSION["id"] = $id;
                                     $_SESSION["username"] = $username;                    
